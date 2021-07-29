12d16d1eeaa79e3405d84965a62a3bc8675fdee0
Derek M. Robertson <dmr.webdev@gmail.com>
2021-07-29 00:45:16 -0700

    Delete projects directory
    Accidental addition, removed as unneeded.

--- projects/microCenter/routes/urlShortener.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express')
-const urlShortenerRouter = Router();
-
-urlShortenerRouter.post('/api/shorturl', urlHandler.addUrl);
-urlShortenerRouter.get('/api/shorturl/:shurl', urlHandler.processShortUrl);
-
-module.exports = urlShortenerRouter;
-
